b1a231655c84827c2370866ff841dcf7bcbc56c4
danielbom <daniel.rodrigues.45@hotmail.com>
2019-06-20 18:41:48 -0300

    Refatorando e colocando alguns comentários.

--- Game/main.lua
+++ Game/main.lua
@@ -7,44 +7,52 @@ local Animation = require("Animation")
 local ManagerAnimations = require("ManagerAnimations")
 
 
-file = "Imagens/background.png"
-background = love.graphics.newImage(file)
-width, height = background:getWidth(), background:getHeight()
-
-backgroundQuad = love.graphics.newQuad(
-    0, 0, width, height,
-    background:getDimensions()
-)
-
 function love.load()
+    -- Carregando o background
+    local image = love.graphics.newImage("Imagens/background.png")
+    width, height = image:getWidth(), image:getHeight()
+    background = {
+        image = image,
+        quad = love.graphics.newQuad( 0, 0,
+            width, height, image:getDimensions()
+        )
+    }
+    -- Definindo a janela
+    canvas = {
+        width = width,
+        height = height,
+    }
+    love.window.setMode(canvas.width, canvas.height, {resizable = true})
+    canvas.format = love.graphics.newCanvas(canvas.width, canvas.height)
+    
+    interface = Interface.New()
+
     pet = Pet.New()
     pet.animations = ManagerAnimations.New()
         .setScale(3)
         .toMiddle()
         .loadAnimations("/Sprites/Togepi/")
-    
-    love.window.setMode(width, height, {resizable = true})
-    canvas = love.graphics.newCanvas(width, height)
-    interface = Interface.New()
 end
 
 function love.update(time)
+    -- Atualizando o pet
     pet.update(time)
-    interface.loadButtons(width, height)
+
+    -- Atualizando a interface
+    interface.loadButtons(canvas.width, canvas.height)
 end
 
 function love.draw()
-    love.graphics.draw(canvas, width, height)
-    love.graphics.draw(background, backgroundQuad)
-    -- for i = 0, love.graphics.getWidth() / background:getWidth() do
-    --     for j = 0, love.graphics.getHeight() / background:getHeight() do
-    --         love.graphics.draw(
-    --             background, i*background:getWidth(), j*background:getHeight()
-    --         )
-    --     end
-    -- end
+    -- Desenhando a janela
+    love.graphics.draw(canvas.format, canvas.width, canvas.height)
+
+    -- Desenhando o background
+    love.graphics.draw(background.image, background.quads)
+
+    -- Desenhando o pet
     pet.displayStatus()
     pet.animations.display()
-    
+
+    -- Desenhando os componentes de interface
     interface.draw()
 end
